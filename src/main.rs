469a13d67d515b3fdbae6069a9e78fee1609d5d6
StarbirdTech <64431622+StarbirdTech@users.noreply.github.com>
2024-01-07 15:38:51 -0500

    improved error handing on camera query and disconnect

--- src/main.rs
+++ src/main.rs
@@ -8,7 +8,7 @@ use nokhwa::{
 #[cfg(target_os = "macos")]
 use nokhwa::nokhwa_initialize;
 
-fn main() {
+fn main() -> Result<(), Box<dyn std::error::Error>> {
     #[cfg(target_os = "macos")]
     nokhwa_initialize(|granted| {
         if !granted {
@@ -17,28 +17,43 @@ fn main() {
         }
     });
 
-    let cameras = query(ApiBackend::Auto).unwrap();
-    cameras.iter().for_each(|cam| println!("{:?}", cam));
+    let cameras: Vec<nokhwa::utils::CameraInfo> = query(ApiBackend::Auto)?;
+    if cameras.is_empty() {
+        eprintln!("No cameras found");
+        return Ok(());
+    } else {
+        println!("Found {} cameras", cameras.len());
+        cameras
+            .iter()
+            .for_each(|cam: &nokhwa::utils::CameraInfo| println!("{:?}", cam));
+    }
 
-    let format = RequestedFormat::new::<RgbFormat>(RequestedFormatType::AbsoluteHighestFrameRate);
+    let format: RequestedFormat<'_> =
+        RequestedFormat::new::<RgbFormat>(RequestedFormatType::AbsoluteHighestFrameRate);
+    let first_camera: &nokhwa::utils::CameraInfo = &cameras[0];
 
-    let first_camera = cameras.first().unwrap();
-
-    let mut threaded = CallbackCamera::new(first_camera.index().clone(), format, |buffer| {
-        let image = buffer.decode_image::<RgbAFormat>().unwrap();
-        println!("{}x{} {}", image.width(), image.height(), image.len());
-    })
+    let mut threaded: CallbackCamera = CallbackCamera::new(
+        first_camera.index().clone(),
+        format,
+        |buffer: nokhwa::Buffer| {
+            let image: image::ImageBuffer<image::Rgba<u8>, Vec<u8>> =
+                buffer.decode_image::<RgbAFormat>().unwrap();
+            println!("{}x{} {}", image.width(), image.height(), image.len());
+        },
+    )
     .unwrap();
     threaded.open_stream().unwrap();
-    #[allow(clippy::empty_loop)] // keep it running
+
     loop {
-        let frame = threaded.poll_frame().unwrap();
-        let image = frame.decode_image::<RgbAFormat>().unwrap();
-        println!(
-            "{}x{} {} naripoggers",
-            image.width(),
-            image.height(),
-            image.len()
-        );
+        match threaded.poll_frame() {
+            Ok(frame) => match frame.decode_image::<RgbAFormat>() {
+                Ok(image) => println!("{}x{} {}", image.width(), image.height(), image.len()),
+                Err(e) => eprintln!("Error decoding image: {}", e),
+            },
+            Err(e) => {
+                eprintln!("Error polling frame: {}", e);
+                return Ok(()); // exit
+            }
+        }
     }
 }
